cdf54006f719b4b144109f963a598a4cb5fbd694
Liu Lantao <liulantao@gmail.com>
2017-08-29 01:30:28 +0800

    publish
    Signed-off-by: Liu Lantao <liulantao@gmail.com>

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -15,10 +15,23 @@ class StoriesController < ApplicationController
   end
 
   def publish
-    @result = params[:summary]
+    @result = params[:summary]  # default
+    @result = twitter_client.update params[:summary] if params[:publish]
 
     respond_to do |format|
       format.js
+    end
+  end
+
+  private
+
+  def twitter_client
+    @twitter_auth = Authentication.first
+    @twitter_client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV.fetch("TWITTER_API_KEY")
+      config.consumer_secret     = ENV.fetch("TWITTER_API_SECRET")
+      config.access_token        = @twitter_auth.token
+      config.access_token_secret = @twitter_auth.secret
     end
   end
 
